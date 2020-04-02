6778df92b8cba73442bc3d5c3650e98147a18ed7
Nichamon <numon1998@gmail.com>
2020-04-02 17:29:07 +0700

    add ui

--- reliever_app/lib/main.dart
+++ reliever_app/lib/main.dart
@@ -1,111 +1,312 @@
-import 'package:flutter/material.dart';
+    import 'package:flutter/material.dart';
+    import 'dart:ui' as ui;
+    import 'package:relieverapp/login.dart';
+    void main() => runApp(MyApp());
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+    class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: MyHomePage(title: 'Reliever Application'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      home : new HomeScreen()
+      );
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  class HomeScreen extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+      return new Scaffold(
+        body: ListView(
+        children: <Widget>[
+          Container(
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        Container(
+        color: Colors.orangeAccent[100],
+        alignment: Alignment.center,
+        width: 420,
+        height: 660,
+        child: CustomPaint(
+          painter: MyPainter(), 
+          child: Align(
+            alignment: Alignment(0.0, 0.1),
+            child: RichText(
+            text: TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              children: <TextSpan>[
+                TextSpan(
+                  text: '  JUST BECAUSE YOU\n',
+                  style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold, decoration: TextDecoration.none),
+                ),
+                    TextSpan(
+                      text: '   HAVE A BAD DAY\n',
+                      style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold,decoration: TextDecoration.none),
+                    ),
+                    TextSpan(
+                      text: '  DOESN\'T MEAN YOU\n',
+                      style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold,decoration: TextDecoration.none),
+                    ),
+                    TextSpan(
+                      text: '   HAVE A BAD LIFE',
+                      style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold,decoration: TextDecoration.none)
+                )
+                
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          )
+          ),
+        ),
+      ),
+          Container(
+              padding: EdgeInsets.fromLTRB(15, 50, 0, 0),
+              child: Text('Welcome', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w300, color: Colors.indigo[700])),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(15, 100, 0, 0),
+              child: Text('to Reliever', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w300, color: Colors.indigo[700])),
+            ),
+            Container(
+              child: Positioned( 
+                left: 300, top: 30, width: 80, height: 80,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/LG5.png")
+                    )
+                ),
+              ) 
+              )
+            ),
+            // ButtonTheme(
+            //   padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            //   minWidth: 300.0,
+            //   height: 30.0,
+            //   child: RaisedButton(
+            //     onPressed: () {},
+            //     child: Text('Let\'s Login\n', 
+            //     style: TextStyle(color: Colors.white, fontSize: 20,)),
+            //     color: Colors.cyan[200],
+            //     shape: RoundedRectangleBorder(
+            //     borderRadius: BorderRadius.circular(20)
+            //   ),
+            //   ),
+            // ),
+
+            // Stack(
+            //   children: [
+            //     Positioned(
+            //       top: 50,
+            //       left: 50,
+            //       child: RaisedButton(
+            //   child: Text('Let\'s Login\n', 
+            //   style: TextStyle(color: Colors.white, fontSize: 20)),
+            //   color: Colors.cyan[200],
+            //   shape: RoundedRectangleBorder(
+            //     borderRadius: BorderRadius.circular(20)
+            //   ),
+            //   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            //   onPressed: (){})
+            //     )
+            //   ],
+            // )
+            Container(
+            width: 500.0,
+            height: 600.0,
+            padding: EdgeInsets.only(left:65, right: 50, top: 550),
+            child: RaisedButton(
+              child: Text('Let\'s Login\n', 
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+              color: Colors.cyan[200],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)
+              ),
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              onPressed: (){
+                Navigator.push(context, 
+                MaterialPageRoute(builder: (context)=> new LogInScreen()));
+              }),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ]
+      )
+      );
+    } 
   }
-}
+//       title: 'Login',
+//       home: Scaffold(
+//       body: ListView(
+//         children: <Widget>[
+//           Container(
+//         child: Stack(
+//           children: <Widget>[
+//         Container(
+//         color: Colors.orangeAccent[100],
+//         alignment: Alignment.center,
+//         width: 420,
+//         height: 700,
+//         child: CustomPaint(
+//           painter: MyPainter(), 
+//           child: Align(
+//             alignment: Alignment(0.0, 0.1),
+//             child: RichText(
+//             text: TextSpan(
+//               style: DefaultTextStyle.of(context).style,
+//               children: <TextSpan>[
+//                 TextSpan(
+//                   text: '    JUST BECAUSE YOU\n',
+//                   style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold),
+//                 ),
+//                     TextSpan(
+//                       text: '       HAVE A BAD DAY\n',
+//                       style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold),
+//                     ),
+//                     TextSpan(
+//                       text: '    DOESN\'T MEAN YOU\n',
+//                       style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold),
+//                     ),
+//                     TextSpan(
+//                       text: '       HAVE A BAD LIFE',
+//                       style: TextStyle(color: Colors.indigo[700], fontSize: 20, fontWeight: FontWeight.bold)
+//                 )              
+//               ],
+//             ),
+//           )
+//           ),
+//         ),
+//       ),
+//           Container(
+//               padding: EdgeInsets.fromLTRB(15, 50, 0, 0),
+//               child: Text('Welcome', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w300, color: Colors.indigo[700])),
+//             ),
+//             Container(
+//               padding: EdgeInsets.fromLTRB(15, 100, 0, 0),
+//               child: Text('to Reliever', style: TextStyle( fontSize: 40, fontWeight: FontWeight.w300, color: Colors.indigo[700])),
+//             ),
+//             Container(
+//               child: Positioned( 
+//                 left: 300, top: 30, width: 80, height: 80,
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     image: DecorationImage(
+//                       image: AssetImage("assets/LG5.png")
+//                     )
+//                 ),
+//               ) 
+//               )
+//             ),
+//             // ButtonTheme(
+//             //   padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+//             //   minWidth: 300.0,
+//             //   height: 30.0,
+//             //   child: RaisedButton(
+//             //     onPressed: () {},
+//             //     child: Text('Let\'s Login\n', 
+//             //     style: TextStyle(color: Colors.white, fontSize: 20,)),
+//             //     color: Colors.cyan[200],
+//             //     shape: RoundedRectangleBorder(
+//             //     borderRadius: BorderRadius.circular(20)
+//             //   ),
+//             //   ),
+//             // ),
+
+//             // Stack(
+//             //   children: [
+//             //     Positioned(
+//             //       top: 50,
+//             //       left: 50,
+//             //       child: RaisedButton(
+//             //   child: Text('Let\'s Login\n', 
+//             //   style: TextStyle(color: Colors.white, fontSize: 20)),
+//             //   color: Colors.cyan[200],
+//             //   shape: RoundedRectangleBorder(
+//             //     borderRadius: BorderRadius.circular(20)
+//             //   ),
+//             //   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+//             //   onPressed: (){})
+//             //     )
+//             //   ],
+//             // )
+//             Container(
+//             width: 500.0,
+//             height: 600.0,
+//             padding: EdgeInsets.only(left:65, right: 50, top: 550),
+//             child: RaisedButton(
+//               child: Text('Let\'s Login\n', 
+//               style: TextStyle(color: Colors.white, fontSize: 20)),
+//               color: Colors.cyan[200],
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(20)
+//               ),
+//               padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+//               onPressed: (){
+//                 Navigator.push(context, 
+//                 MaterialPageRoute(builder: (context)=>LogInScreen()));
+//               }),
+//             )
+//           ],
+//         ),
+//       ),
+//       ]
+//       )
+//       )
+//     );
+//   }
+// }
+
+    // class HomePage extends StatelessWidget {
+    //   @override
+    //   Widget build(BuildContext context) {
+    //     return Scaffold(
+    //       backgroundColor: Colors.orangeAccent[100],
+    //       body: CustomPaint(
+    //         painter: MyPainter(),
+    //       ),
+    //     );
+    //   }
+    // }
+
+    class MyPainter extends CustomPainter{
+      @override
+      void paint(ui.Canvas canvas, ui.Size size) {
+        //final width = size.width;
+        
+        Paint paint = Paint();
+        Path recPath = Path();
+        Path pigPath = Path();
+        Path ovalPath = Path();
+        
+
+        pigPath.moveTo(0, size.height * 0.417);
+        pigPath.quadraticBezierTo(size.width * 0.25, size.height * 0.6,
+            size.width * 0.5, size.height * 0.37);
+        pigPath.quadraticBezierTo(size.width * 0.7, size.height * 0.23,
+            size.width * 1.0, size.height * 0.2);
+        pigPath.lineTo(size.width, size.height);
+        pigPath.lineTo(0, size.height);
+        paint.color = Colors.cyan[100];
+        canvas.drawPath(pigPath, paint);
+
+        ovalPath.moveTo(0, size.height * 0.3);
+        ovalPath.quadraticBezierTo(size.width * 0.55, size.height * 0.3,
+            size.width * 0.77, size.height * 0.625);
+        ovalPath.quadraticBezierTo(size.width * 0.87, size.height * 0.7284,
+            size.width * 1.0, size.height * 0.5767);
+        ovalPath.lineTo(size.width, size.height);
+        ovalPath.lineTo(0, size.height);
+        paint.color = Colors.indigo[700];
+        canvas.drawPath(ovalPath, paint);
+
+        recPath.addRRect(
+          RRect.fromRectAndRadius(Rect.fromLTWH(size.width / 7, size.height / 2.4, size.width * 3 / 4, size.height/ 4), Radius.circular(22))
+        );
+        paint.color = Colors.white.withOpacity(0.8);
+        canvas.drawPath(recPath, paint);
+      }
+
+      @override
+      bool shouldRepaint(CustomPainter oldDelegate) => false;
+      
+    }
--- reliever_app/test/widget_test.dart
+++ reliever_app/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:relieverapp/main.dart';
-
+//import 'package:relieverapp/login.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
