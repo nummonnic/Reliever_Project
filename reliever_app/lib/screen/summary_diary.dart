b530de81a58082cce0b1a37d18d0edcc0b09a618
tangpnm <tang_6528@hotmail.com>
2020-11-14 21:41:14 +0700

    send data to db, it already has the date in voice

--- reliever_app/lib/screen/summary_diary.dart
+++ reliever_app/lib/screen/summary_diary.dart
@@ -93,10 +93,10 @@ class _SummaryDiaryState extends State<SummaryDiary> {
                     width: MediaQuery.of(context).size.width,
                     child: Padding(
                       padding: const EdgeInsets.only(
-                        top: 40,
+                        top: 20,
                         left: 30,
                         right: 30,
-                        bottom: 40,
+                        bottom: 20,
                       ),
                       child: Container(
                         child: Column(
@@ -110,18 +110,18 @@ class _SummaryDiaryState extends State<SummaryDiary> {
                                   "Date:",
                                   style: TextStyle(
                                     fontWeight: FontWeight.w500,
-                                    fontSize: 25,
+                                    fontSize: 20,
                                     color: Colors.black,
                                   ),
                                 ),
                                 SizedBox(
-                                  height: 20,
+                                  height: 10,
                                 ),
                                 Text(
                                   selectedDate,
                                   style: TextStyle(
                                     // fontWeight: FontWeight.w500,
-                                    fontSize: 20,
+                                    fontSize: 15,
                                     color: Colors.black,
                                   ),
                                 ),
@@ -134,18 +134,18 @@ class _SummaryDiaryState extends State<SummaryDiary> {
                                   "Diary:",
                                   style: TextStyle(
                                     fontWeight: FontWeight.w500,
-                                    fontSize: 25,
+                                    fontSize: 20,
                                     color: Colors.black,
                                   ),
                                 ),
                                 SizedBox(
-                                  height: 20,
+                                  height: 10,
                                 ),
                                 Text(
                                   diary,
                                   style: TextStyle(
                                     // fontWeight: FontWeight.w500,
-                                    fontSize: 20,
+                                    fontSize: 15,
                                     color: Colors.black,
                                   ),
                                 ),
@@ -158,18 +158,18 @@ class _SummaryDiaryState extends State<SummaryDiary> {
                                   "Emotion: ",
                                   style: TextStyle(
                                     fontWeight: FontWeight.w500,
-                                    fontSize: 25,
+                                    fontSize: 20,
                                     color: Colors.black,
                                   ),
                                 ),
                                 SizedBox(
-                                  height: 20,
+                                  height: 10,
                                 ),
                                 Text(
                                   emotion,
                                   style: TextStyle(
                                     // fontWeight: FontWeight.w500,
-                                    fontSize: 20,
+                                    fontSize: 15,
                                     color: Colors.black,
                                   ),
                                 ),
@@ -195,8 +195,8 @@ class _SummaryDiaryState extends State<SummaryDiary> {
                                               child:
                                                   CircularProgressIndicator())
                                           : Container(
-                                              height: 80,
-                                              width: double.infinity,
+                                              height: 60,
+                                              width: 150,
                                               // color: Color(0xff27496d),
                                               child: MaterialButton(
                                                 onPressed: () async {
@@ -261,175 +261,5 @@ class _SummaryDiaryState extends State<SummaryDiary> {
         ),
       ),
     );
-    // return Scaffold(
-    //   body: Container(
-    //     // color: Color(0xff27496d),
-    //     child: Column(
-    //       crossAxisAlignment: CrossAxisAlignment.start,
-    //       children: <Widget>[
-    //         Container(
-    //           height: MediaQuery.of(context).size.height / 2,
-    //           decoration: BoxDecoration(
-    //             color: Color(0xff27496d),
-    //             borderRadius: BorderRadius.only(
-    //               bottomLeft: Radius.circular(40),
-    //               bottomRight: Radius.circular(40),
-    //             ),
-    //           ),
-    //           child: SafeArea(
-    //             child: Container(
-    //               child: Column(
-    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                 crossAxisAlignment: CrossAxisAlignment.start,
-    //                 children: <Widget>[
-    //                   Column(
-    //                     crossAxisAlignment: CrossAxisAlignment.start,
-    //                     children: <Widget>[
-    //                       Row(
-    //                         children: <Widget>[
-    //                           IconButton(
-    //                             icon: Icon(
-    //                               Icons.arrow_back_ios,
-    //                               color: Colors.white,
-    //                               size: 20,
-    //                             ),
-    //                             onPressed: () {
-    //                               Navigator.pop(context);
-    //                             },
-    //                           ),
-    //                         ],
-    //                       ),
-    //                       Padding(
-    //                         padding: const EdgeInsets.only(
-    //                           left: 30,
-    //                         ),
-    //                         child: Text(
-    //                           "My Diary",
-    //                           style: TextStyle(
-    //                             fontWeight: FontWeight.w900,
-    //                             fontSize: 30,
-    //                             color: Colors.white,
-    //                           ),
-    //                         ),
-    //                       ),
-    //                     ],
-    //                   ),
-    //                   Padding(
-    //                     padding: const EdgeInsets.only(
-    //                       left: 60,
-    //                     ),
-    //                     child: Column(
-    //                       crossAxisAlignment: CrossAxisAlignment.start,
-    //                       children: <Widget>[
-    //                         Text(
-    //                           "Date",
-    //                           style: TextStyle(
-    //                             fontWeight: FontWeight.w700,
-    //                             fontSize: 20,
-    //                             color: Colors.white,
-    //                           ),
-    //                         ),
-    //                         SizedBox(
-    //                           height: 10,
-    //                         ),
-    //                         Text(
-    //                           selectedDate,
-    //                           // "19 January 2020",
-    //                           style: TextStyle(
-    //                             fontWeight: FontWeight.w300,
-    //                             fontSize: 17,
-    //                             color: Colors.white,
-    //                           ),
-    //                         ),
-    //                       ],
-    //                     ),
-    //                   ),
-    //                   Padding(
-    //                     padding: const EdgeInsets.only(
-    //                       left: 60,
-    //                       bottom: 50,
-    //                     ),
-    //                     child: Column(
-    //                       crossAxisAlignment: CrossAxisAlignment.start,
-    //                       children: <Widget>[
-    //                         Text(
-    //                           "Diary",
-    //                           style: TextStyle(
-    //                             fontWeight: FontWeight.w700,
-    //                             fontSize: 20,
-    //                             color: Colors.white,
-    //                           ),
-    //                         ),
-    //                         SizedBox(
-    //                           height: 10,
-    //                         ),
-    //                         diary != ""
-    //                             ? Text(
-    //                                 // "long sentence",
-    //                                 diary,
-    //                                 style: TextStyle(
-    //                                   fontWeight: FontWeight.w300,
-    //                                   fontSize: 17,
-    //                                   color: Colors.white,
-    //                                 ),
-    //                               )
-    //                             : Text(
-    //                                 "Please Enter Diary",
-    //                                 // diary,
-    //                                 style: TextStyle(
-    //                                   fontWeight: FontWeight.w300,
-    //                                   fontSize: 17,
-    //                                   color: Colors.white,
-    //                                 ),
-    //                               )
-    //                       ],
-    //                     ),
-    //                   ),
-    //                 ],
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         Padding(
-    //           padding: const EdgeInsets.all(20.0),
-    //           child: Container(
-    //             child: Column(
-    //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //               crossAxisAlignment: CrossAxisAlignment.start,
-    //               children: <Widget>[
-    //                 Text(
-    //                   "Your emotion: ",
-    //                   style: TextStyle(
-    //                     fontWeight: FontWeight.w900,
-    //                     fontSize: 25,
-    //                     // color: Colors.white,
-    //                   ),
-    //                 ),
-    //                 Text(
-    //                   emotion,
-    //                   style: TextStyle(
-    //                     fontWeight: FontWeight.w900,
-    //                     fontSize: 25,
-    //                     // color: Colors.white,
-    //                   ),
-    //                 ),
-    //                 SizedBox(
-    //                   height: 20,
-    //                 ),
-    //                 Center(
-    //                   child: Container(
-    //                     height: 100,
-    //                     width: 100,
-    //                     color: Colors.amber,
-    //                   ),
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
